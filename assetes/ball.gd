a71c3707449b7055237c4f13f7719d0c180313d9
KADRI Youssef <contact@with-us.fr>
2023-11-20 13:40:27 +0100

    Ajout de la fleche de direction

--- assetes/ball.gd
+++ assetes/ball.gd
@@ -5,18 +5,21 @@ extends RigidBody3D
 @onready var progressBar = get_node("/root/Bowling/Progress_Bar/ProgressBar")
 
 # Vitesse de base à laquelle la balle est lancée
-var base_speed = 10
+var base_speed = 1
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
 		launch_ball()
 
 func launch_ball():
-	# Direction basée sur la rotation de la flèche
-	var direction = Vector3(cos(arrow.rotation.y), 0, sin(arrow.rotation.y))
+	# Direction basée sur la transformation de la flèche
+	var direction = arrow.global_transform.basis.x
+	direction.y = 0  # Fixe la composante y de la direction à 0
 	
 	# Force basée sur la valeur de la barre de progression
-	var force_multiplier = progressBar.value / 100.0
-	var force = direction * base_speed * force_multiplier
+	var force_multiplier = progressBar.value*2
+	var force = direction.normalized() * base_speed * force_multiplier
 	
 	self.apply_central_impulse(force)
+
+
